e13dfbd73891c13b3c0e7206fc9c0fb59fb1f5e7
rubansi <rubansi24@gmail.com>
2025-09-14 11:48:54 +0300

    task 4

---
+++ 3-value_argument.js
@@ -0,0 +1,15 @@
+// Write a script that prints the first argument passed to it:
+
+// - If no arguments are passed to the script, print “No argument”
+// - You must use `console.log(...)` to print all output
+// - You are not allowed to use `var`
+// - You are not allowed to use `length`
+
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log("No argument");
+} else {
+  console.log(args[0]);
+}       
+
